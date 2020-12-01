c4c8e1e779bdee2b6c2568fda5df95f02816b8a9
Ben Elferink <ben.elferink@icloud.com>
2020-12-01 17:22:17 +0200

    SVG's - cleaned confusing lines

--- src/media/icons/backspace.jsx
+++ src/media/icons/backspace.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Icon(props) {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' className={props.className} onClick={props.onClick} x='0' y='0' enableBackground='new 0 0 44.18 44.18' version='1.1' viewBox='0 0 44.18 44.18' xmlSpace='preserve'>
+    <svg xmlns='http://www.w3.org/2000/svg' className={props.className} onClick={props.onClick} viewBox='0 0 44 44'>
       <g fill={props.backspaceBg}>
         <path d='M11.18 6.09L43.18 6.09 43.18 38.09 11.18 38.09 1.18 22.09z'></path>
         <path d='M44.18 39.09H10.625L0 22.09l10.625-17H44.18v34zm-32.446-2H42.18v-30H11.734l-9.375 15 9.375 15z'></path>
