bf7330c1a0e2c7be6b761176186790ad0c37169f
Ben Elferink <ben.elferink@icloud.com>
2021-02-11 16:34:07 +0200

    future apps

--- src/layout/Navbar/index.js
+++ src/layout/Navbar/index.js
@@ -3,6 +3,8 @@ import NavItem from '../../components/NavItem';
 import HomeIcon from '../../icons/Home';
 import QueueIcon from '../../icons/Queue';
 import EmailIcon from '../../icons/Email';
+// import TwitterIcon from '../../icons/Twitter';
+// import WhatsAppIcon from '../../icons/WhatsApp';
 
 export default function Navbar() {
   return (
@@ -11,6 +13,8 @@ export default function Navbar() {
       <NavItem title='Home' path='/' Icon={HomeIcon} />
       <NavItem title='Queue' path='/queue' Icon={QueueIcon} />
       <NavItem title='Gmail' path='/gmail' Icon={EmailIcon} />
+      {/* <NavItem title='Twitter' path='/twitter' Icon={TwitterIcon} /> */}
+      {/* <NavItem title='WhatsApp' path='/whatsapp' Icon={WhatsAppIcon} /> */}
     </nav>
   );
 }
